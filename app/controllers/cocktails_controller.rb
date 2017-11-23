954f3c0f1aaa6497ca7be4d4af8e256d93f1677a
Manuel Farez <manufarez@gmail.com>
2017-11-23 14:23:02 +0100

    Rails new with frontend and form gems

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,50 @@
+class CocktailsController < ApplicationController
+  def index         # GET /cocktails
+    @cocktails = Cocktail.all
+  end
+
+  def show          # GET /cocktails/:id
+    @cocktail = Cocktail.find(params[:id])
+    # @review = Review.new
+  end
+
+  def new           # GET /cocktails/new
+    @cocktail = Cocktail.new
+  end
+
+  def create        # POST /cocktails
+    @cocktail = Cocktail.new
+    @cocktail.save
+
+    # no need for app/views/cocktails/create.html.erb
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  # def edit          # GET /cocktails/:id/edit
+  #   @cocktail = Cocktail.find(params[:id])
+  # end
+
+  # def update        # PATCH /cocktails/:id
+  #   @cocktail = Cocktail.find(params[:id])
+  #   @cocktail.update(cockatil_params)
+
+  #   # no need for app/views/cocktails/create.html.erb
+  #   redirect_to cocktail_path(@cocktail)
+  # end
+
+  # def destroy       # DELETE /cocktails/:id
+  #   @cocktail = Cocktail.find(params[:id])
+  #   @cocktail.destroy
+
+  #   # no need for app/views/cocktails/create.html.erb
+  #   redirect_to cocktails_path
+  # end
+
+  private
+
+  def cocktail_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:cocktail).permit(:name)
+  end
+end
